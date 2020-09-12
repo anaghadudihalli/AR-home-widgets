4387cc2e75f107c94eece6ed04912a637491d606
Anagha Dudihalli <adudih2@uic.edu>
2020-09-12 09:38:54 -0500

    Time widget

---
+++ Assets/getTime.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class getTime : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject timeTextObject;
+    void Start()
+    {
+        InvokeRepeating("UpdateTime", 0f, 30f);
+    }
+
+    // Update is called once per frame
+    void UpdateTime()
+    {
+        timeTextObject.GetComponent<TextMeshPro>().text = System.DateTime.Now.ToString("h:mm tt");
+    }
+}
