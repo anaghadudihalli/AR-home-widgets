130911854e7d0a026b38fde53b279b80f2fa2b5c
Anagha Dudihalli <adudih2@uic.edu>
2020-09-12 10:34:16 -0500

    Add date widget

---
+++ Assets/getDate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class getDate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public TextMeshPro dateTextObject;
+    public TextMeshPro dayTextObject;
+    public TextMeshPro monthYearTextObject;
+    
+    void Start()
+    {
+        InvokeRepeating("UpdateDate", 0f, 30f);
+    }
+
+    // Update is called once per frame
+    void UpdateDate()
+    {
+        dateTextObject.GetComponent<TextMeshPro>().text = System.DateTime.Now.ToString("dd");
+        dayTextObject.GetComponent<TextMeshPro>().text = System.DateTime.Now.ToString("dddd");
+        monthYearTextObject.GetComponent<TextMeshPro>().text = System.DateTime.Now.ToString("MMMM, yyyy");
+    }
+}
