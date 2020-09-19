d885d1f75429d241a985353617a494be6fa7f1eb
Anagha Dudihalli <adudih2@uic.edu>
2020-09-19 00:25:49 -0500

    Change time units on button press

--- Assets/changeUnits.cs
+++ Assets/changeUnits.cs
@@ -3,21 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using Vuforia;
+using TMPro;
+
 
 public class changeUnits : MonoBehaviour
 {
     // Start is called before the first frame update
+    public getTime getTimeObject; 
     public GameObject buttonObject;
+    public GameObject buttonTextObject;
+    public static int isMetric; 
     void Start()
     {
         buttonObject = GameObject.Find("ChangeUnitsButton");
         buttonObject.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(OnButtonPressed);
         buttonObject.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(OnButtonReleased);
+        isMetric = 0;
     }
 
     public void OnButtonPressed(VirtualButtonBehaviour vb)
     {
         Debug.Log("Button pressed");
+        if (isMetric == 0) {
+            isMetric = 1;
+            buttonTextObject.GetComponent<TextMeshPro>().text = "Change to Imperial Units";
+            getTimeObject.callUpdate();
+        }
+        else {
+            isMetric = 0;
+            buttonTextObject.GetComponent<TextMeshPro>().text = "Change to Metric Units";
+            getTimeObject.callUpdate();
+        }
+        
     }
 
     public void OnButtonReleased(VirtualButtonBehaviour vb)
--- Assets/getTime.cs
+++ Assets/getTime.cs
@@ -7,14 +7,28 @@ public class getTime : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject timeTextObject;
+    public string formattedString;
+    public int getUnits;
+
     void Start()
-    {
+    {   
         InvokeRepeating("UpdateTime", 0f, 30f);
+    }
+
+    public void callUpdate() {
+        UpdateTime();
     }
 
     // Update is called once per frame
     void UpdateTime()
     {
-        timeTextObject.GetComponent<TextMeshPro>().text = System.DateTime.Now.ToString("h:mm tt");
+        getUnits = changeUnits.isMetric;
+        if (getUnits == 1) {
+            formattedString = "HH:mm";
+        }
+        else {
+            formattedString = "hh:mm tt";
+        }
+        timeTextObject.GetComponent<TextMeshPro>().text = System.DateTime.Now.ToString(formattedString);
     }
 }
