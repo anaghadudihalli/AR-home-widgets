883464150af573190295975eb49f2ccfab993842
Anagha Dudihalli <adudih2@uic.edu>
2020-09-18 23:16:46 -0500

    Add button

---
+++ Assets/changeUnits.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Vuforia;
+
+public class changeUnits : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject buttonObject;
+    void Start()
+    {
+        buttonObject = GameObject.Find("ChangeUnitsButton");
+        buttonObject.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(OnButtonPressed);
+        buttonObject.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(OnButtonReleased);
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Button pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Button Released");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
